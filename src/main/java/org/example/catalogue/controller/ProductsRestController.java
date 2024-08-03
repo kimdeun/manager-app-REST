5f50f330e537e484c350ea8d2d2d28147510fe25
De Un Kim <deunkim@MacbookM1.local>
2024-08-03 12:30:09 +0300

    rest app

---
+++ src/main/java/org/example/catalogue/controller/ProductsRestController.java
@@ -0,0 +1,47 @@
+package org.example.catalogue.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.example.catalogue.controller.payload.NewProductPayload;
+import org.example.catalogue.entity.Product;
+import org.example.catalogue.service.ProductService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("catalogue-api/products")
+public class ProductsRestController {
+    private final ProductService productService;
+
+    @GetMapping
+    public List<Product> findProducts() {
+        return productService.findAllProducts();
+    }
+
+    @PostMapping
+    public ResponseEntity<?> createProduct(@Valid @RequestBody NewProductPayload payload,
+                                                 BindingResult bindingResult,
+                                                 UriComponentsBuilder uriComponentsBuilder) throws BindException{
+        if (bindingResult.hasErrors()) {
+            if (bindingResult instanceof BindException exception) {
+                throw exception;
+            } else {
+                throw new BindException(bindingResult);
+            }
+        } else {
+            Product product = productService.createProduct(payload.title(), payload.details());
+            return ResponseEntity
+                    .created(uriComponentsBuilder
+                            .replacePath("/catalogue-api/products/{productId}")
+                            .build(Map.of("productId", product.getId())))
+                    .body(product);
+        }
+    }
+}
---
+++ src/main/java/org/example/catalogue/repository/ProductRepository.java
@@ -0,0 +1,16 @@
+package org.example.catalogue.repository;
+
+import org.example.catalogue.entity.Product;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductRepository {
+    List<Product> findAll();
+
+    Product save(Product product);
+
+    Optional<Product> findById(Integer productId);
+
+    void deleteById(Integer id);
+}

4c2a255f2aeb3e8eb44c0f271cead3c1da6e1b97
De Un Kim <deunkim@MacbookM1.local>
2024-08-13 17:14:55 +0300

    added oauth2 server

--- src/main/java/org/example/catalogue/config/SecurityBeans.java
+++ src/main/java/org/example/catalogue/config/SecurityBeans.java
@@ -2,8 +2,10 @@ package org.example.catalogue.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -12,12 +14,21 @@ public class SecurityBeans {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .authorizeHttpRequests(authorizeRequests ->
-                        authorizeRequests.requestMatchers("/catalogue-api/**")
-                                .hasRole("SERVICE"))
-                .httpBasic(Customizer.withDefaults())
-                .sessionManagement(sessionManagement -> sessionManagement
+                .authorizeHttpRequests(authorizeRequests -> authorizeRequests
+                        .requestMatchers(HttpMethod.POST, "/catalogue-api/products")
+                        .hasAuthority("SCOPE_edit_catalogue")
+                        .requestMatchers(HttpMethod.PATCH, "/catalogue-api/products/{productId}")
+                        .hasAuthority("SCOPE_edit_catalogue")
+                        .requestMatchers(HttpMethod.DELETE, "/catalogue-api/products/{productId}")
+                        .hasAuthority("SCOPE_edit_catalogue")
+                        .requestMatchers(HttpMethod.GET)
+                        .hasAuthority("SCOPE_view_catalogue")
+                        .anyRequest().denyAll())
+                .csrf(CsrfConfigurer::disable)
+                .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .oauth2ResourceServer(oauth2ResourceServer -> oauth2ResourceServer
+                        .jwt(Customizer.withDefaults()))
                 .build();
     }
 }
